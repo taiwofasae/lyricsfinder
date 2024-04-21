82634af44aff122bf866204494123ffd3390b0bc
Taiwo Fasae <taiwofasae@gmail.com>
2024-04-21 00:59:59 -0500

    reorganize folders

--- admin/flask/contact.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-contact_bp = Blueprint('contact', __name__)
-
-@contact_bp.route('/hello/')
-def hello():
-    return "Hello from Contact Page"
--- admin/flask/home.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-home_bp = Blueprint('home', __name__)
-
-@home_bp.route('/hello/')
-def hello():
-    return "Hello from Home Page"
