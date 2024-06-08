076f2a829e4152bbbf8ec9c0b4a9e5c395177fbf
Taiwo Fasae <taiwofasae@gmail.com>
2024-06-08 17:58:06 -0500

    Stop weird embeddings fetch on app start

--- web/lyricsproject/lyricsapp/apps.py
+++ web/lyricsproject/lyricsapp/apps.py
@@ -4,8 +4,8 @@ class LyricsappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'lyricsapp'
 
-    def ready(self):
-        # from scheduler import scheduler
-        # scheduler.start()
-        from lyricsapp import dispatchers
-        dispatchers.fetch_embeddings(batch_size=20)
+    # def ready(self):
+    #     # from scheduler import scheduler
+    #     # scheduler.start()
+    #     from lyricsapp import dispatchers
+    #     dispatchers.fetch_embeddings(batch_size=20)
